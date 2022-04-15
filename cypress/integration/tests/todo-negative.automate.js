945f094897c11714ec0d6b2d6c0e441b33ba433d
Dusan Maravic <dusan.maravic@gmail.com>
2022-04-15 15:53:17 +0200

    added some negative TC

---
+++ cypress/integration/tests/todo-negative.automate.js
@@ -0,0 +1,46 @@
+/// <reference types='cypress' />
+
+describe('toDo list negative test', () => {
+    const numbers = 1010101010
+    const specChar = '@$#%^^^#@!!@##'
+    const emptyString = ''
+    const emptySpace = ' '
+    const undefined = null
+
+    beforeEach(() => {
+        cy.visit('https://todomvc.com/examples/angular2/')
+    })
+
+    it('create numbers todo item', () => {
+
+        cy.get('.new-todo').type(`${numbers}{enter}`)
+        .first()
+        cy.get('.todo-list li').should('contain', numbers)
+    })
+
+    it('create special char todo item', () => {
+
+        cy.get('.new-todo').type(`${specChar}{enter}`)
+        .first()
+        cy.get('.todo-list li').should('contain', specChar)
+    })
+
+    it('create empty string todo item', () => {
+
+        cy.get('.new-todo').type(`${emptyString}{enter}`)
+        .should('contain', emptyString)
+        expect(emptyString).to.be.empty
+    })
+
+    // it('create empty space string todo item', () => {
+
+    //     cy.get('.new-todo').type(`${emptySpace}{enter}`)
+    //     .should('contain', emptySpace)
+    // })
+
+    it('create undefined todo item', () => {
+        cy.get('.new-todo').type(`${undefined}{enter}`)
+        cy.get('.todo-list li')
+        expect(undefined).to.be.null.value
+    })
+})
--- cypress/integration/tests/todo-positive.automate.js
+++ cypress/integration/tests/todo-positive.automate.js
@@ -1,6 +1,6 @@
 /// <reference types='cypress' />
 
-describe('toDo list test', () => {
+describe('toDo list positive test', () => {
   const task = 'Create test'
   const task2 = 'Edit test'
   const task3 = 'Complete test'
@@ -8,6 +8,27 @@ describe('toDo list test', () => {
 
   beforeEach(() => {
     cy.visit('https://todomvc.com/examples/angular2/')
+
+    // cy.get('.new-todo').type(`${task}{enter}`)
+    // cy.get('.todo-list li')
+    // .should('contain', task)
+    // cy.get('.new-todo').type(`${task2}{enter}`)
+    // cy.get('.todo-list li')
+    // .should('contain', task2)
+    // cy.get('.new-todo').type(`${task3}{enter}`)
+    // cy.get('.todo-list li')
+    // .should('contain', task3)
+    // cy.get('.new-todo').type(`${task4}{enter}`)
+    // cy.get('.todo-list li')
+    // .should('contain', task4)
+
+    // cy.get('.todo-list li')
+    // .should('have.length', 4)
+    // .last()
+    // .should('include.text', task4)
+  })
+
+  it('can create todo items', () => {
 
     cy.get('.new-todo').type(`${task}{enter}`)
     cy.get('.todo-list li')
@@ -21,57 +42,45 @@ describe('toDo list test', () => {
     cy.get('.new-todo').type(`${task4}{enter}`)
     cy.get('.todo-list li')
     .should('contain', task4)
-
     cy.get('.todo-list li')
     .should('have.length', 4)
-    .last()
-    .should('include.text', task4)
   })
-
-  // it('can create todo items', () => {
-  //   cy.get('.new-todo').type(`${task}{enter}`)
-  //   cy.get('.todo-list li')
-  //   .should('contain', task)
-  //   cy.get('.new-todo').type(`${task2}{enter}`)
-  //   cy.get('.todo-list li')
-  //   .should('contain', task2)
-  //   cy.get('.new-todo').type(`${task3}{enter}`)
-  //   cy.get('.todo-list li')
-  //   .should('contain', task3)
-  //   cy.get('.new-todo').type(`${task4}{enter}`)
-  //   cy.get('.todo-list li')
-  //   .should('contain', task4)
-
-  //   cy.get('.todo-list li')
-  //   .should('have.length', 4)
-  // })
 
   it('can edit all todo items', () => {
 
+    cy.get('.new-todo').type(`${task}{enter}`)
     cy.get('.todo-list li').contains(task).dblclick()
     cy.get('.edit').clear()
     cy.get('.edit').type('First task edited{enter}')
     cy.get('.todo-list li').should('contain', 'First task')
 
+    cy.get('.new-todo').type(`${task2}{enter}`)
     cy.get('.todo-list li').contains(task2).dblclick()
     cy.get('.edit').clear()
     cy.get('.edit').type('Second task edited{enter}')
     cy.get('.todo-list li').should('contain', 'Second task')
 
+    cy.get('.new-todo').type(`${task3}{enter}`)
     cy.get('.todo-list li').contains(task3).dblclick()
     cy.get('.edit').clear()
     cy.get('.edit').type('Third task edited{enter}')
     cy.get('.todo-list li').should('contain', 'Third task edited')
 
+    cy.get('.new-todo').type(`${task4}{enter}`)
     cy.get('.todo-list li').contains(task4).dblclick()
     cy.get('.edit').clear()
     cy.get('.edit').type('Fourth task edited{enter}')
     cy.get('.todo-list li').should('contain', 'Fourth task edited')
-
     cy.get('.todo-list li').should('have.length', 4)
   })
 
   it('can complete all tasks', () => {
+
+    cy.get('.new-todo').type(`${task}{enter}`)
+    cy.get('.new-todo').type(`${task2}{enter}`)
+    cy.get('.new-todo').type(`${task3}{enter}`)
+    cy.get('.new-todo').type(`${task4}{enter}`)
+
     cy.get('.toggle').check()
     cy.get('.footer').should('contain', '0 items left')
     cy.get('.clear-completed').click()
@@ -80,6 +89,12 @@ describe('toDo list test', () => {
   })
 
   it('can delete all tasks', () => {
+
+    cy.get('.new-todo').type(`${task}{enter}`)
+    cy.get('.new-todo').type(`${task2}{enter}`)
+    cy.get('.new-todo').type(`${task3}{enter}`)
+    cy.get('.new-todo').type(`${task4}{enter}`)
+
     cy.get('.todo-list li').contains(task)
     .next()
     .click({force: true})
